52cd802a8fbd2e1b9c326e3a9c33d108ea7a0b09
Bibiresanmi Oluwagbenga David <davidbibiresanmi@gmail.com>
2026-04-30 11:59:20 +0100

    simi is beautiful

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import sitemap from 'vite-plugin-sitemap';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    historyApiFallback: true,
-  }
-})
-
+  plugins: [
+    // other plugins like react()
+    sitemap({ 
+      hostname: 'https://www.stageprophi.app',
+      dynamicRoutes: ['/events', '/about', '/contact'] 
+    }),
+  ],
+});
